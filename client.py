52cd36b761095a44af0f7ac2a13ceaaa236d1721
Margaret Jagger <margaretjagger2@gmail.com>
2022-05-01 13:31:10 -0400

    Change client retrieval of guess results

--- client.py
+++ client.py
@@ -1,10 +1,7 @@
 from socket import *
-import sys
 
-serverName = '140.103.93.138'  # put IP address
-# serverName = '140.103.97.87'
+serverName = 'localhost' # put IP address
 serverPort = 13456
-#
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
@@ -14,38 +11,27 @@ currentGuess = ''
 numGuesses = 6
 playAgain = False
 
-try:
-	while numGuesses != 0:
-		try:
-			while True:
-				numGuesses -= 1
-				currentGuess = str(input("Enter a letter to guess: "))
+while True:
+	currentGuess = str(input("Enter a letter to guess: "))
 
-				# Client-side guess validation
-				if not currentGuess.isalpha():
-					print("Enter only a letter")
-					continue
-				if currentGuess in lettersGuessed:
-					print("You already guessed this letter. Try again.")
-					continue
+	# Client-side guess validation
+	if not currentGuess.isalpha():
+		print("Enter only a letter")
+		continue
+	if currentGuess in lettersGuessed:
+		print("You already guessed this letter. Try again.")
+		continue
+	if len(currentGuess) > 1:
+		print("You have too many letters.")
 
-				lettersGuessed += currentGuess
-				bytesGuess = bytes(currentGuess, "utf-8")
-				clientSocket.sendto(bytesGuess, (serverName, serverPort))
-				print("Sent message")
-				modifiedMsg, server = clientSocket.recvfrom(2048)
-		# guess is not the right argument type or is more than 1 letter
-		except ValueError or len(currentGuess) > 1:
-			print("Enter only one letter.")
-			continue
-	playStatus = str(input("Want to play again? Y/N"))
-	if playStatus == 'Y':
-		playAgain = True
-		numGuesses = 6
-	if playStatus == 'N':
-		sys.exit("Game quit")
+	lettersGuessed += currentGuess
+	clientSocket.send(bytes(currentGuess, "utf-8"))
+	letterMsg = clientSocket.recv(1024).decode("utf-8")
+	print("How many guesses left? " + str(letterMsg))
 
-except KeyboardInterrupt:
-	sys.exit("Game quit")
+	if str(letterMsg) == 0:
+		print("LOSE GAME")
+		clientSocket.close()
+		break
 
 clientSocket.close()
