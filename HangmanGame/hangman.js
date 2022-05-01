d8985eb765272f84edf178f195ca45c3e4ee256a
Emily <epfau22@wooster.edu>
2022-04-30 23:43:45 -0400

    picture appear
    images will appear not correctly.

--- HangmanGame/hangman.js
+++ HangmanGame/hangman.js
@@ -1,11 +1,13 @@
 let numberOfGuesses = 6; // number of guesses left
 // let wonGame = False;
 let possibleWords = ["cat", "dog", "fish"];
+let guessInProgress = "";
 let missedLetters = [];
 let correctLetters = [];
 let imageSources = ["pics/hangmandesigns.jpg", "pics/hangman0Lives.jpg", "pics/hangman1Lives.jpg",
     "pics/hangman2Lives.jpg", "pics/hangman3Lives.jpg", "pics/hangman4Lives.jpg", "pics/hangman4Lives.jpg",
     "pics/hangman6Lives.jpg"];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var theWord = {};
 var holdChar;
 
@@ -15,12 +17,27 @@ function randomWord(possibleWords){
     theWord.currentWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
 }
 
+//alphabet buttons
+function alphabetButton(){
+
+    // allButtons = document.getElementById("buttons");
+    // letters = document.createElement("ul");
+    //
+    // for (var i = 0; i <= alphabet.length; i++){
+    //     letters.id = 'alphabet';
+    //     letterButton = document.createElement("char");
+    //     letterButton.id = 'letters';
+    //     letterButton.innerHTML = alphabet[i];
+    //     allButtons.body.appendChild(letters);
+    //     letters.body.appendChild(letterButton);
+    // }
+}
 
 //one letter from alphabet function taken a compare to randomWord() if there is a match it is correct if not
 //one numberOFGuess goes down and picture changes
 function holdLetter(holdChar){
-    document.getElementById("HangManPic").innerHTML = theWord.currentWord; //for testing you can see the word
-    changeImage();
+    document.getElementById("forPractice").innerHTML = theWord.currentWord;
+    changeImage(numberOfGuesses);
 
     if (theWord.currentWord.indexOf(holdChar) != -1){
         correctLetters.push(holdChar);
@@ -40,33 +57,30 @@ function holdLetter(holdChar){
 
 }
 //picture from imageSources change according to the numberOfGuesses
-function changeImage(){
-    var picture = document.createElement("picture");
+function changeImage(numberOfGuesses){
 
-    switch (numberOfGuesses) {
-        case 1:
-            picture.src = "pics/hangmandesigns.jpg";
-            break;
-        case 2:
-            picture.src = "pics/hangman5Lives.jpg";
-            break;
-        case 3:
-            picture.src = "pics/hangman4Lives.jpg";
-            break;
-        case 4:
-            picture.src = "pics/hangman3Lives.jpg";
-            break;
-        case 5:
-            picture.src = "pics/hangman1Lives.jpg";
-            break;
-        case 6:
-            picture.src = "pics/hangman0Lives.jpg";
-            break;
-        default:
-            picture.src = imageSources[0];
-    var div = document.getElementById("HangManPic");
-    div.appendChild(picture);
+    const img = document.createElement('img');
+
+    if(numberOfGuesses == 1) {
+        img.src = "../pics/hangman1Lives.jpg";
     }
+    if(numberOfGuesses == 2) {
+        img.src = "../pics/hangman2Lives.jpg";
+    }
+    if(numberOfGuesses == 3) {
+        img.src = "../pics/hangman3Lives.jpg";
+    }
+    if(numberOfGuesses == 4) {
+        img.src = "../pics/hangman4Lives.jpg";
+    }
+    if(numberOfGuesses == 5) {
+        img.src = "../pics/hangman5Lives.jpg";
+    }
+    if(numberOfGuesses >= 6) {
+        img.src = "../pics/hangman6Lives.jpg";
+    }
+    document.getElementById("HangManPic").appendChild(img);
+
 }
 
  function playGame(){
